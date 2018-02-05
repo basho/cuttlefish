edec8b4b082307ab94682084ea79d86819ef166b
bryanhuntesl <bryan.hunt@erlang-solutions.com>
2018-02-05 22:28:46 +0000

    prep for test boot

---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule Cuttlefish.Mixfile do
+  use Mix.Project
+
+  #@version File.read!("VERSION") |> String.strip
+
+  def project do
+    [app: :cuttlefish,
+     version: "0.1.0",
+     elixir: "~> 1.5.2",
+     description: description(),
+     escript: escript(),
+    deps: dependencies()]
+  end
+
+  defp dependencies() do
+    [
+      {:lager, "~> 3.6"}
+    ]
+  end
+
+  defp escript do
+    [main_module: :cuttlefish_escript]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp package do
+    [files: ~w(src lib mix.exs priv rebar.config README.textile LICENSE VERSION),
+     contributors: ["Sean Cribbs"],
+     licenses: ["MIT License"],
+     links: %{"GitHub" => "https://github.com/seancribbs/neotoma"}]
+  end
+
+  defp description do
+    """
+    Erlang library and packrat parser-generator for parsing expression grammars.
+    """
+  end
+
+end
