7942f850cdfa7f2dd2b8e3d1103fa8672183d73a
Luke Bakken <luke@bakken.io>
2020-11-30 15:10:29 -0800

    Fixup copyrights

--- src/conf_parse.erl
+++ src/conf_parse.erl
@@ -20,6 +20,8 @@
 %% conf_parse: for all your .conf parsing needs.
 %%
 %% Copyright (c) 2013 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2019 Pivotal Software, Inc.  All rights reserved.
+%% Copyright (c) 2020 VMware, Inc. or its affiliates.  All rights reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
